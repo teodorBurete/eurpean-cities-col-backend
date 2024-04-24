4295342ed864d67fc951ecab0a74204e71a9e2b7
teodorBurete <teodor.burete@yahoo.com>
2024-04-24 12:27:15 +0300

    asd

---
+++ src/main/java/com/europeancol/licenta/europeancitiescol/EuropeanCitiesColApplication.java
@@ -0,0 +1,13 @@
+package com.europeancol.licenta.europeancitiescol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropeanCitiesColApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuropeanCitiesColApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/europeancol/licenta/europeancitiescol/EuropeanCitiesColApplicationTests.java
@@ -0,0 +1,13 @@
+package com.europeancol.licenta.europeancitiescol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuropeanCitiesColApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
